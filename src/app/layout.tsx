7578ed95d152a74aee809cbe5ebbaf07f68729a9
Carlos <carlosforjadores@gmail.com>
2023-08-03 02:01:32 -0700

    next render components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-      <ToastContainer />
+      <body className={inter.className}>
+        {children}
+        <ToastContainer />
+      </body>
     </html>
   );
 }
